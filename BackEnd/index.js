2949f2a50ae62da6227fb12a53deab5226e9ab12
Narsimmayemul <narsimmayemul49450@gmail.com>
2024-10-11 12:14:47 +0530

    changes done

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -30,7 +30,10 @@ app.get("/" , (req ,res)=>{
     }
 })
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+  
 app.listen( PORT , ()=>{
     try {
         if(connection){
